d3a40e4fabbc29d00d228b47e783a2cf65897a1f
Tomas <tomasoldenburg@hotmail.com>
2023-06-10 09:20:19 -0300

    (fixed)error in axiosapi

--- client/src/api/trutests.api.js
+++ client/src/api/trutests.api.js
@@ -8,33 +8,30 @@ const URL =
 // Importaciones relacionadas con Trutests
 const trutestsUrl = `${URL}/trutests/api/v1/trutests`;
 const TrutestsApi = useAxios();
-const TrutestsInstance = TrutestsApi.create();
 
 // Importaciones relacionadas con Files
 const fileUrl = `${URL}/trutests/api/v1/file`;
 const FileApi = useAxios();
-const FileInstance = FileApi.create();
 
 // Importaciones relacionadas con Animals
 const animalUrl = `${URL}/trutests/api/v1/animal`;
 const AnimalApi = useAxios();
-const AnimalInstance = AnimalApi.create();
 
 // Funciones de la API
-export const getAllTrutests = () => TrutestsInstance.get(trutestsUrl);
-export const getTrutest = (id) => TrutestsInstance.get(`${trutestsUrl}/${id}`);
+export const getAllTrutests = () => TrutestsApi.get(trutestsUrl);
+export const getTrutest = (id) => TrutestsApi.get(`${trutestsUrl}/${id}`);
 export const createTrutest = (trutest) =>
-  TrutestsInstance.post(trutestsUrl + "/", trutest);
+  TrutestsApi.post(trutestsUrl + "/", trutest);
 export const updateTrutest = (id, trutest) =>
-  TrutestsInstance.put(`${trutestsUrl}/${id}/`, trutest);
+  TrutestsApi.put(`${trutestsUrl}/${id}/`, trutest);
 export const deleteTrutest = (id) =>
-  TrutestsInstance.delete(`${trutestsUrl}/${id}`);
+  TrutestsApi.delete(`${trutestsUrl}/${id}`);
 
 export const uploadTrutest = (file) => {
   const formData = new FormData();
   formData.append("file", file);
-  return FileInstance.post(fileUrl + "/", formData);
+  return FileApi.post(fileUrl + "/", formData);
 };
 
 export const createAnimal = (animal) =>
-  AnimalInstance.post(animalUrl + "/", animal);
+  AnimalApi.post(animalUrl + "/", animal);
